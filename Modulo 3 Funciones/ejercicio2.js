1aacc006b311d6950adbfe1abd87234483e2384c
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-24 17:23:35 -0600

    Ejercicio 2

---
+++ Modulo 3 Funciones/ejercicio2.js
@@ -0,0 +1,6 @@
+//Veamos si se va entendiendo: escribí ahora una función con nombre  mitad, que tome un número y devuelva su mitad.
+function mitad(numero){
+    return numero / 2;
+}
+
+console.log(mitad(4));
