bc6f3bd39d776e3b11e5f08724379d7bfe5c1f79
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-24 17:44:10 -0600

    Ejercicio 5

---
+++ Modulo 3 Funciones/ejercicio5.js
@@ -0,0 +1,11 @@
+//Funcionception
+function tripleDeLaSuma(numero1,numero2){
+    let resultadoDeSuma = numero1 + numero2;
+    return triple(resultadoDeSuma);
+}
+
+function triple(numero3){
+    return numero3 * 3;
+}
+
+console.log(tripleDeLaSuma(1,2));
