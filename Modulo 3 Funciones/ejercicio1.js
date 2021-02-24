203efda92135c8dd89ad137b33c18ec5aada244a
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-24 17:19:49 -0600

    Ejercicio 1

---
+++ Modulo 3 Funciones/ejercicio1.js
@@ -0,0 +1,6 @@
+//Prueba declarando a la función triple, la cual triplique el valor pasado por parámetro y nos devuelva su resultado.	
+function triple(numero){
+    return 3 * numero;
+}
+
+console.log(triple(2));
