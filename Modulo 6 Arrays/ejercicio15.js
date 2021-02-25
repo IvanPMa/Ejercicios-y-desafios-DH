de96f02ec5e0c910bc00d0c76cbef5a5450f5f5a
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-25 16:34:09 -0600

    M6 Ejercicio 15 Fixed

--- Modulo 6 Arrays/ejercicio15.js
+++ Modulo 6 Arrays/ejercicio15.js
@@ -1,4 +1,13 @@
-
-console.log(cantidadDeMesesConGanancia([]));
-console.log(cantidadDeMesesConGanancia([10,20,30,-20]));
-console.log(cantidadDeMesesConGanancia([]));
+function cantidadDeMesesConGanancia(unPeriodo) {
+    let cantidad = 0;
+    for (let i = 0; i < unPeriodo.length;i++) {
+      if (unPeriodo[i] > 0){
+        cantidad=cantidad+1;
+        }     
+    }
+    return cantidad;
+  }
+  
+  console.log(cantidadDeMesesConGanancia([]));
+  console.log(cantidadDeMesesConGanancia([10,20,30,-20]));
+  console.log(cantidadDeMesesConGanancia([]));
