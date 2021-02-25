6ba424479a7e7c9b727cfcb946310bbb058a8ee2
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-24 18:53:38 -0600

    M4 Ejercicio 3

---
+++ Modulo 4 Condicionales/ejercicio3.js
@@ -0,0 +1,11 @@
+function elMayor(numero1,numero2){
+    if(numero1 > numero2){
+        return numero1;
+    }
+    else{
+        return numero2;
+    }
+}
+
+console.log(elMayor(4,5));
+console.log(elMayor(10,4));
