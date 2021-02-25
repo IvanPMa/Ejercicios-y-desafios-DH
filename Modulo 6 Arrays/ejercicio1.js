3add075c41092415203429637b34423042a52d7f
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-25 11:00:27 -0600

    M6 Ejercicio 1 y 2

---
+++ Modulo 6 Arrays/ejercicio1.js
@@ -0,0 +1,7 @@
+let seriesFavoritasDeAna = ["Game of Thrones", "Breaking Bad", "House of Cards"]; 
+let seriesFavoritasDeHector = ["En Terapia", "Recordando el show de Alejandro Molina"];
+
+console.log(seriesFavoritasDeAna);
+console.log(seriesFavoritasDeHector),
+console.log(["hola","mundo!"]);
+console.log(["hola","hola!"]);
---
+++ Modulo 6 Arrays/ejercicio2.js
@@ -0,0 +1,8 @@
+let arraysIguales =(["hola", "mundo"], ["mundo", "hola"]);
+arraysIguales =(["hola", "mundo"], ["hola", "mundo"]);
+arraysIguales =(["hola", "mundo"], ["hola", "todo", "el", "mundo"]);
+arraysIguales =(["hola"], ["hola", "mundo"]);
+console.log(["hola", "mundo"] === ["mundo", "hola"]);
+let personas;
+console.log(["mara", "julian"] === personas);
+console.log(personas === personas);
