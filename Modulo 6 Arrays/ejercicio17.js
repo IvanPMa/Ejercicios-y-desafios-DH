b867ee1370e6adeca79053a4daca8e44c0a179ef
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-25 16:44:46 -0600

    M6 ejercicio 17

--- Modulo 6 Arrays/ejercicio17.js
+++ Modulo 6 Arrays/ejercicio17.js
@@ -1,8 +1,8 @@
 function cantidadDeMesesConPerdida(unPeriodo){
     let cantidad = 0;
-    for (let i = 0; i < unPeriodo.length; i++){
+    for (let i = 0; i < unPeriodo.length ; i++){
         if (unPeriodo[i] < 0){
-            cantidad = cantidad + 1;
+            cantidad = cantidad +1;
         }
     }
     return cantidad;
