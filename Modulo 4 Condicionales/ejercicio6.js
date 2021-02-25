55c6cf02f80f27da5accf78479ad256621764f6a
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-24 19:49:11 -0600

    M4 Ejercicio 6

---
+++ Modulo 4 Condicionales/ejercicio6.js
@@ -0,0 +1,7 @@
+function filosofoHipster(profesion, nacionalidad,kilometros){
+    return profesion == "Músico" && nacionalidad == "Argentina" && kilometros >=2;
+}
+
+console.log(filosofoHipster("Ingeniero","Mexico",5));
+console.log(filosofoHipster("Músico","Argentina",6));
+console.log(filosofoHipster("Programador","España",1));
