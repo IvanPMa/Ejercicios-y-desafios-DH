830222c8769ee0ad24aa13178602808f2a8a6086
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-24 22:49:34 -0600

    M5 Ejercicio 5

---
+++ Modulo 5 Ciclos/ejercicio5.js
@@ -0,0 +1,9 @@
+function pasandoPorLosPares(){
+    for (let i = 0;i <= 6 ; i++){
+        if (i%2 == 0 ){
+            console.log("acá i tiene el valor de "+i);
+        }
+    }
+}
+
+pasandoPorLosPares();
